3a3707d46167c0f44fbec83954b6df8b6d985170
sebpearce <sebpearce@gmail.com>
2016-10-03 22:43:31 +1100

    Start tests for expense transactions controller

--- spec/controllers/expense_transactions_controller_spec.rb
+++ spec/controllers/expense_transactions_controller_spec.rb
@@ -1,7 +1,26 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe ExpenseTransactionsController do
-  it 'blah' do
-    true
+  describe 'GET index' do
+    it 'responds with a non-zero array of objects' do
+      get :index
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.size).to be > 0
+    end
+  end
+
+  describe '#show' do
+    context 'when no start date or end date is provided' do
+      it { raise 'TODO' }
+    end
+    context 'when a start date is provided but no end date' do
+      it { raise 'TODO' }
+    end
+    context 'when no start date is provided but an end date is' do
+      it { raise 'TODO' }
+    end
+    context 'when both a start and end date are provided' do
+      it { raise 'TODO' }
+    end
   end
 end
